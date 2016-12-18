f6bb84578e26da72791e2b75ba7213ebdd38bc69
Robert Shippey <robert@robertshippey.net>
2016-12-18 02:12:56 +0000

    add sorting to admin events list

--- IBDE-plugin/IBDE-plugin.php
+++ IBDE-plugin/IBDE-plugin.php
@@ -1,40 +1,31 @@
 <?php 
-/*
-Plugin Name: IBD Events Functionality
-*/
+/**
+ * Plugin Name: IBD Events Functionality
+ */
 
 $plugin_path = plugin_dir_path( __FILE__ );
 
+// Imports API and other keys
 require_once ($plugin_path . '_keys.php');
 
 require_once ($plugin_path . 'event-class.php');
 require_once ($plugin_path . 'timezones.php');
-
 require_once ($plugin_path . 'helper-functions.php');
 require_once ($plugin_path . 'comments.php');
 require_once ($plugin_path . 'dashboard.php');
-
 require_once ($plugin_path . 'archives.php');
-
 require_once ($plugin_path . 'save-event-hook.php');
-
 require_once ($plugin_path . 'ical-generator.php');
 
 require_once ($plugin_path . 'post-type.php');
 register_activation_hook( __FILE__, 'ibde_add_roles_on_plugin_activation' );
 
-
 require_once ($plugin_path . 'plugin-activation/plugin-activation-config.php');
-
 require_once ($plugin_path . 'stripe-api/init.php');
-
 require_once ($plugin_path . 'cleanup-things.php');
-
 require_once ($plugin_path . 'opengraph.php');
-
 require_once ($plugin_path . 'weather.php');
-
 require_once ($plugin_path . 'cron.php');
 require_once ($plugin_path . 'taxonomy-count.php');
-
 require_once ($plugin_path . 'news.php');
+require_once ($plugin_path . 'admin_sort.php');
---
+++ IBDE-plugin/admin_sort.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Admin Sorting hooks
+ */
+
+/**
+ * Adds Start Date column into Event admin list
+ */
+add_filter( 'manage_posts_columns', 'ibde_managing_my_event_columns', 10, 2 );
+function ibde_managing_my_event_columns( $columns, $post_type ) {
+
+  if ( $post_type == 'ibde-event' ){
+
+    unset($columns['date']);
+
+    $new_columns = array();
+    foreach( $columns as $key => $value ) {
+      $new_columns[ $key ] = $value;
+      if ( $key == 'title' )
+       $new_columns[ 'ibde_start_date_column' ] = 'Start Date';
+   }
+   return $new_columns;
+ }
+ return $columns;
+}
+
+/** 
+ * Populates each row with the events start date
+ */
+add_action( 'manage_posts_custom_column', 'ibde_populating_my_event_columns', 10, 2 );
+function ibde_populating_my_event_columns( $column_name, $post_id ) {
+
+ switch( $column_name ) {
+  case 'ibde_start_date_column':
+
+  $start_date_string = get_post_meta( $post_id, 'start_date_utc', true );
+  $start_date_obj = new DateTime($start_date_string, new DateTimeZone("UTC") );
+
+  $start_date_tz_string = get_post_meta( $post_id, 'timezone', true );
+  $output_tz_obj = new DateTimeZone($start_date_tz_string);
+
+  $start_date_obj->setTimezone( $output_tz_obj );
+
+  echo $start_date_obj->format('Y-m-d H:i');
+
+  break;
+}
+}
+
+/** 
+ * Registers Start Date column to be sortable
+ */
+add_filter( 'manage_edit-ibde-event_sortable_columns', 'ibde_manage_sortable_columns', PHP_INT_MAX );
+function ibde_manage_sortable_columns( $sortable_columns ) {
+
+ $sortable_columns[ 'ibde_start_date_column' ] = 'order_start_date';
+
+ return $sortable_columns;
+}
+
+/** 
+ * Sets the query to order by start date when set in admin
+ */
+add_action( 'pre_get_posts', 'ibde_event_admin_ordering_query', 99 );
+function ibde_event_admin_ordering_query( $query ) {
+
+ if ( $query->is_main_query() && ( $orderby = $query->get( 'orderby' ) ) ) {
+
+  switch( $orderby ) {
+
+   case 'order_start_date':
+
+   $query->set( 'meta_key', 'start_date_utc' );
+
+   $query->set( 'orderby', 'meta_value' );
+
+   break;
+
+ }
+}
+}
